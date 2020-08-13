c7c68ad828e4656518b1a8ef5b89c60b5e7136f7
C.W. Chien <dppss92132@gmail.com>
2020-08-13 22:30:05 +0800

    add an easy example of self implented event system in NodeJs without using event module.

---
+++ NodeJs/EZ_Self_implemented_event_system.js
@@ -0,0 +1,27 @@
+function Test(){
+    this.event_listeners = {
+        'func1' : (listener) => {
+            console.log("func1 event default function call");
+        },
+        'func2' : (listener) => {
+            console.log("func2 event default function call");
+        }
+    }
+}
+
+Test.prototype.on = function(event_name, listener){
+    this.event_listeners[event_name] = listener;
+}
+
+Test.prototype.emitEventListener = function(event_name, ...params) {
+  return this.event_listeners[event_name].apply(null, params);
+}
+
+const test = new Test();
+
+test.on("func1", () => {
+    console.log("emitted by func1 event")
+});
+
+test.emitEventListener("func1");
+test.emitEventListener("func2");
